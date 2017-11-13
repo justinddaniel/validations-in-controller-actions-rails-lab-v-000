34f5ac6dd5fb5c34a3275129e5879486b7a542c8
Justin Daniel <justinddaniel@gmail.com>
2017-11-13 20:55:21 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,10 +11,10 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     if @post.valid?
       @post.update(post_params)
-    redirect_to post_path(@post)
-  else
-    render :edit
-  end
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
